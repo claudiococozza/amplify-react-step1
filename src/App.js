468f005db454cbce7acb76c4d9205457c785f8f2
Claudio Cocozza <claudio.cocozza@outlook.it>
2023-06-13 13:54:03 +0200

    JS fix

--- src/App.js
+++ src/App.js
@@ -2,6 +2,13 @@ import React, { useState, useEffect } from "react";
 import "./App.css";
 import "@aws-amplify/ui-react/styles.css";
 import { API, Storage} from "aws-amplify";
+
+import { listNotes } from "./graphql/queries";
+import {
+       createNote as createNoteMutation,
+       deleteNote as deleteNoteMutation,
+} from "./graphql/mutations";
+
 import {
   Button,
   Flex,
@@ -12,11 +19,7 @@ import {
   View,
   withAuthenticator,
 } from "@aws-amplify/ui-react";
-import { listNotes } from "./graphql/queries";
-import {
-  createNote as createNoteMutation,
-  deleteNote as deleteNoteMutation,
-} from "./graphql/mutations";
+
 
 const App = ({ signOut }) => {
   const [notes, setNotes] = useState([]);
@@ -89,63 +92,49 @@ const App = ({ signOut }) => {
             variation="quiet"
             required
           />
+          <View
+                            name="image"
+                            as="input"
+                            type="file"
+                            style={{ alignSelf: "end" }}
+                          />
           <Button type="submit" variation="primary">
             Create Note
           </Button>
         </Flex>
+
       </View>
       <Heading level={2}>Current Notes</Heading>
       <View margin="3rem 0">
+
         {notes.map((note) => (
-          <Flex
-            key={note.id || note.name}
-            direction="row"
-            justifyContent="center"
-            alignItems="center"
-          >
-            <Text as="strong" fontWeight={700}>
-              {note.name}
-            </Text>
-            <Text as="span">{note.description}</Text>
-            <Button variation="link" onClick={() => deleteNote(note)}>
-              Delete note
-            </Button>
-          </Flex>
-        ))}
+            <Flex
+              key={note.id || note.name}
+              direction="row"
+              justifyContent="center"
+              alignItems="center"
+            >
+              <Text as="strong" fontWeight={700}>
+                {note.name}
+              </Text>
+              <Text as="span">{note.description}</Text>
+              {note.image && (
+                <Image
+                  src={note.image}
+                  alt={`visual aid for ${notes.name}`}
+                  style={{ width: 400 }}
+                />
+              )}
+              <Button variation="link" onClick={() => deleteNote(note)}>
+                Delete note
+              </Button>
+            </Flex>
+          ))}
+
       </View>
-      <View
-        name="image"
-        as="input"
-        type="file"
-        style={{ alignSelf: "end" }}
-      />
+
       <Button onClick={signOut}>Sign Out</Button>
     </View>
   );
 };
-
-{notes.map((note) => (
-  <Flex
-    key={note.id || note.name}
-    direction="row"
-    justifyContent="center"
-    alignItems="center"
-  >
-    <Text as="strong" fontWeight={700}>
-      {note.name}
-    </Text>
-    <Text as="span">{note.description}</Text>
-    {note.image && (
-      <Image
-        src={note.image}
-        alt={`visual aid for ${notes.name}`}
-        style={{ width: 400 }}
-      />
-    )}
-    <Button variation="link" onClick={() => deleteNote(note)}>
-      Delete note
-    </Button>
-  </Flex>
-))}
-
-export default withAuthenticator(App);(App);
+export default withAuthenticator(App);
